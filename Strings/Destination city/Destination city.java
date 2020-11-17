8f6efaf0216656e681d0307c5d28759379ad456b
santech1995 <sanjanac777@gmail.com>
2020-11-16 17:08:14 -0800

    Destination city

---
+++ Strings/Destination city/Destination city.java
@@ -0,0 +1,41 @@
+/*Example 1:
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+Example 2:
+
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are: 
+"D" -> "B" -> "C" -> "A". 
+"B" -> "C" -> "A". 
+"C" -> "A". 
+"A". 
+Clearly the destination city is "A".
+Example 3:
+
+Input: paths = [["A","Z"]]
+Output: "Z"
+ 
+
+Constraints:
+
+1 <= paths.length <= 100
+paths[i].length == 2
+1 <= cityAi.length, cityBi.length <= 10
+cityAi != cityBi
+All strings consist of lowercase and uppercase English letters and the space character.*/
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet<>();
+        for(List<String> l : paths) {
+            set.add(l.get(1));
+        }
+        for(List<String> l : paths) {
+            set.remove(l.get(0));
+        }
+        return set.iterator().next();
+    }
+}
