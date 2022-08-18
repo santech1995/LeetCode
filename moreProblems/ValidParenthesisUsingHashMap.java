3a8feb659f2b4345f23e3a58b5f4cf6c79057325
santech1995 <sanjanac777@gmail.com>
2022-08-17 22:45:34 -0700

    Valid parenthesis using hash map

---
+++ moreProblems/ValidParenthesisUsingHashMap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character,Character> maps=new HashMap<Character,Character>();
+        maps.put(')','(');
+        maps.put(']','[');
+        maps.put('}','{');
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(maps.containsKey(c)){
+                if(stack.empty()||stack.pop()!=maps.get(c))return false;
+            }
+            else
+                stack.push(c);
+        }
+        return stack.empty();
+    }
+}
