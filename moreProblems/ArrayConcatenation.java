e4dd37f5b0b75de76c70c6b74a99d2aefa2995f2
santech1995 <sanjanac777@gmail.com>
2022-09-20 06:24:08 -0700

    array concatenation

---
+++ moreProblems/ArrayConcatenation.java
@@ -0,0 +1,13 @@
+//Array concatenation
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length*2;
+        int ans[] = new int[n];
+        for(int i=0; i<nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
