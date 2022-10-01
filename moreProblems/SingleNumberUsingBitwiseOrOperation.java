8d6d476308513ab2d4423cfb809f3fbfbca7572b
santech1995 <sanjanac777@gmail.com>
2022-09-30 22:43:23 -0700

    SingleNumberUsingBitwiseOrOperation

---
+++ moreProblems/SingleNumberUsingBitwiseOrOperation.java
@@ -0,0 +1,11 @@
+//SingleNumberUsingBitwiseOrOperation
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    int a = 0;
+    for (int i : nums) {
+      a ^= i;
+    }
+    return a;
+  }
+}
