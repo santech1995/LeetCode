40246d66b318ce33f72f2aad06f09ec7e06df9b9
santech1995 <sanjanac777@gmail.com>
2023-01-21 15:21:55 -0800

    reverse a string in java

---
+++ PayPal/reverseAString.java
@@ -0,0 +1,18 @@
+//Reverse a string
+import java.io.*;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+        char temp = ' ';
+        while(i < j) {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(s);
+    }
+}
