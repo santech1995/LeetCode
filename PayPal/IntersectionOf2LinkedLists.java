2dee2207fe20276804eab74c077feb18e601e976
santech1995 <sanjanac777@gmail.com>
2023-01-25 23:37:27 -0800

    Intersection of two linked lists

---
+++ PayPal/IntersectionOf2LinkedLists.java
@@ -0,0 +1,12 @@
+//Intersection of 2 linked lists:-
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
